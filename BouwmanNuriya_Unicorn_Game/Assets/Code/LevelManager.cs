c7983566719daf51d88d8d95435d15afe32291f4
NuriyaBouwman <nuriya98@gmail.com>
2018-05-29 19:07:11 +0200

    BPW les 29-05-18
    worked on camera movement and spawning objects

---
+++ BouwmanNuriya_Unicorn_Game/Assets/Code/LevelManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+	CameraMoving,
+	Playing
+}
+
+public class LevelManager : MonoBehaviour 
+{
+	private static LevelManager instance;
+	public static LevelManager Instance
+	{
+		get 
+		{
+			return instance;
+		}
+	}
+
+	public GameObject obstaclePrefab;
+
+	private List<GameObject> obstacles;
+
+	public GameState state = GameState.CameraMoving;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		obstacles = new List<GameObject> ();
+
+		for(int i = 0; i < 5; ++i)
+		{
+			GameObject obstacle = Instantiate(obstaclePrefab, new Vector3(i, 2, 0), Quaternion.identity);
+
+			obstacles.Add (obstacle); 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
